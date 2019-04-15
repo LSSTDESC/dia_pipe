2eba5e82bd7a4c2a6476a94616cdd3bc808c8fb8
Bob Armstrong <rearmstr@gmail.com>
2019-04-15 21:08:28 +0200

    Allow associationDriver to be given an id list to use

--- python/lsst/dia/pipe/associationDriver.py
+++ python/lsst/dia/pipe/associationDriver.py
@@ -10,7 +10,9 @@ from lsst.pipe.base import ArgumentParser, TaskRunner
 from lsst.ctrl.pool.parallel import BatchPoolTask
 from lsst.ctrl.pool.pool import Pool, abortOnError
 from lsst.pipe.drivers.utils import getDataRef, TractDataIdContainer
-from lsst.pipe.tasks.coaddBase import getSkyInfo
+from lsst.pipe.tasks.coaddBase import getSkyInfo, WcsSelectImagesTask
+from lsst.pipe.tasks.coaddBase import SelectDataIdContainer
+from lsst.pipe.base import Struct
 
 from .simple_association import SimpleAssociationTask
 
@@ -28,6 +30,10 @@ class AssociationDriverConfig(Config):
         doc="Task used to associate DiaSources with DiaObjects.",
     )
     ccdKey = Field(dtype=str, default='detector', doc="Name of ccd to give to butler")
+    select = ConfigurableField(
+        doc="Image selection subtask.",
+        target=WcsSelectImagesTask,
+    )
 
 
 class AssociationDriverTaskRunner(TaskRunner):
@@ -52,6 +58,10 @@ class AssociationDriverTaskRunner(TaskRunner):
         else:
             raise RuntimeError("parsedCmd or args must be specified")
         return self.TaskClass(config=self.config, log=self.log, butler=butler)
+
+    @staticmethod
+    def getTargetList(parsedCmd, **kwargs):
+        return TaskRunner.getTargetList(parsedCmd, selectDataList=parsedCmd.selectId.dataList, **kwargs)
 
 
 def unpickle(factory, args, kwargs):
@@ -88,6 +98,7 @@ class AssociationDriverTask(BatchPoolTask):
         BatchPoolTask.__init__(self, **kwargs)
         self.butler = butler
         self.makeSubtask("associator")
+        self.makeSubtask("select")
         self.schema = afwTable.SourceTable.makeMinimalSchema()
 
     def __reduce__(self):
@@ -103,6 +114,9 @@ class AssociationDriverTask(BatchPoolTask):
         parser.add_id_argument("--id", "deepCoadd",
                                help="data ID, e.g. --id tract=12345 patch=1,2 filter=g^r^i",
                                ContainerClass=TractDataIdContainer)
+        parser.add_id_argument("--selectId", "deepDiff_differenceExp",
+                               help="data ID, e.g. --selectId visit=6789 ccd=0..9",
+                               ContainerClass=SelectDataIdContainer)
         return parser
 
     @classmethod
@@ -119,7 +133,7 @@ class AssociationDriverTask(BatchPoolTask):
         return time*numTargets/float(numCpus)
 
     @abortOnError
-    def runDataRef(self, patchRefList):
+    def runDataRef(self, patchRefList, selectDataList=[]):
         """!Run association processing on coadds
 
         Only the master node runs this method.
@@ -151,37 +165,19 @@ class AssociationDriverTask(BatchPoolTask):
                 patches[patch] = []
             patches[patch].append(dataId)
 
-        pool.map(self.runAssociation, patches.values())
+        pool.map(self.runAssociation, patches.values(), selectDataList)
 
-    def runAssociation(self, cache, dataIdList):
-        """! Run association on a patch
-        For all of the visits that overlap this patch in the band create a DIAObject
-        catalog.  Only the objects in the non-overlaping area of the tract and patch
-        are included.
+    def catalogGenerator(self, cache, dataRefList, selectDataList=[]):
+        """! Get a generator of difference images from the visit catalof for each datRef
         """
-
-        dataRefList = [getDataRef(cache.butler, dataId, self.config.coaddName + "Coadd_calexp") for
-                       dataId in dataIdList]
-
-        idFactory = None
-
         for dataRef in dataRefList:
-
             try:
                 calexp = dataRef.get(f"{self.config.coaddName}Coadd_calexp")
             except Exception:
-                self.log.info('Cannot read data for %s' % (dataRef.dataId))
+                self.log.info('Cannot coadd read data for %s' % (dataRef.dataId))
                 continue
 
-            coaddWcs = calexp.getWcs()
             visitCatalog = calexp.getInfo().getCoaddInputs().ccds
-            band = dataRef.dataId['filter']
-            skyInfo = getSkyInfo(coaddName=self.config.coaddName, patchRef=dataRef)
-
-            innerPatchBox = afwGeom.Box2D(skyInfo.patchInfo.getInnerBBox())
-            self.log.info('Total number of images from %s %s %s to read %d' %
-                          (dataRef.dataId['tract'], dataRef.dataId['patch'], band, len(visitCatalog)))
-
             for visitRec in visitCatalog:
 
                 visit = visitRec.get('visit')
@@ -190,53 +186,123 @@ class AssociationDriverTask(BatchPoolTask):
                 try:
                     exp = cache.butler.get(f"{self.config.coaddName}Diff_differenceExp", dataId)
                     src = cache.butler.get(f"{self.config.coaddName}Diff_diaSrc", dataId)
-                    srcWcs = exp.getWcs()
                 except Exception as e:
-                    self.log.debug('Cannot read data for %d %d. skipping %s' % (visit, ccd, e))
+                    self.log.debug('Cannot read difference data for %d %d. skipping %s' % (visit, ccd, e))
                     continue
+                data = Struct(visit=visit, ccd=ccd, exp=exp, src=src,
+                              filter=dataRef.dataId['filter'])
+                yield data
 
-                if idFactory is None:
-                    expBits = dataRef.get("deepMergedCoaddId_bits")
-                    expId = int(dataRef.get("deepMergedCoaddId"))
-                    idFactory = afwTable.IdFactory.makeSource(expId, 64 - expBits)
-                    self.associator.initialize(src.schema, idFactory)
+    def idListGenerator(self, cache, dataRefList, selectDataList=[]):
+        """! Get a generator of difference images from the selectDataList
+        """
+        skyInfo = getSkyInfo(coaddName=self.config.coaddName, patchRef=dataRefList[0])
+        diffExpRefList = self.selectExposures(dataRefList[0], skyInfo, selectDataList=selectDataList)
+        for dataRef in diffExpRefList:
+            try:
+                exp = dataRef.get(f"{self.config.coaddName}Diff_differenceExp")
+                src = dataRef.get(f"{self.config.coaddName}Diff_diaSrc")
+            except Exception as e:
+                self.log.debug('Cannot read data for %d %d. skipping %s' % (dataRef.dataId['visit'],
+                                                                            dataRef.dataId['ccd'], e))
+                continue
+            data = Struct(visit=dataRef.dataId['visit'],
+                          ccd=dataRef.dataId[self.config.ccdKey],
+                          filter=dataRef.dataId['filter'], exp=exp, src=src)
+            yield data
 
-                mask = np.array(
-                    [innerPatchBox.contains(coaddWcs.skyToPixel(srcWcs.pixelToSky(a.getCentroid())))
-                     for a in src],
-                    dtype=bool
-                )
+    def runAssociation(self, cache, dataIdList, selectDataList):
+        """! Run association on a patch
+        For all of the visits that overlap this patch in the band create a DIAObject
+        catalog.  Only the objects in the non-overlaping area of the tract and patch
+        are included.
+        """
+        dataRefList = [getDataRef(cache.butler, dataId, self.config.coaddName + "Coadd_calexp") for
+                       dataId in dataIdList]
 
-                src = src[mask]
-                if len(src) == 0:
-                    continue
+        # We need the WCS for the patch, so we can use the first entry in the dataIdList
+        dataRef = dataRefList[0]
+        skyInfo = getSkyInfo(coaddName=self.config.coaddName, patchRef=dataRef)
+        try:
+            calexp = dataRef.get(f"{self.config.coaddName}Coadd_calexp")
+        except Exception:
+            self.log.info('Cannot read coadd data for %s' % (dataRef.dataId))
+            return
 
-                self.log.info('Reading difference image %d %d, %s with %d possible sources' %
-                              (visit, ccd, dataRef.dataId['filter'], len(src)))
+        coaddWcs = calexp.getWcs()
+        innerPatchBox = afwGeom.Box2D(skyInfo.patchInfo.getInnerBBox())
 
-                footprints = []
-                region = calexp.getBBox(afwImage.PARENT)
-                for ii, rec in enumerate(src):
-                    # transformations on large footprints can take a long time
-                    # We truncate the footprint since we will rarely be interested
-                    # in such large footprints
-                    if rec.getFootprint().getArea() > self.config.maxFootprintArea:
-                        spans = afwGeom.SpanSet.fromShape(self.config.defaultFootprintRadius,
-                                                          afwGeom.Stencil.CIRCLE,
-                                                          afwGeom.Point2I(rec.getCentroid()))
-                        foot = afwDet.Footprint(spans)
-                        foot.addPeak(int(rec.getX()), int(rec.getY()), 1)
-                    else:
-                        foot = rec.getFootprint()
-                    footprints.append(foot.transform(srcWcs, coaddWcs, region))
+        expBits = dataRef.get("deepMergedCoaddId_bits")
+        expId = int(dataRef.get("deepMergedCoaddId"))
+        idFactory = afwTable.IdFactory.makeSource(expId, 64 - expBits)
 
-                self.associator.addCatalog(src, band, visit, ccd, footprints)
+        if len(selectDataList) == 0:
+            differenceImages = self.catalogGenerator
+        else:
+            differenceImages = self.idListGenerator
+
+        initializeSelector = False
+        for diffIm in differenceImages(cache, dataRefList, selectDataList):
+
+            if initializeSelector is False:
+                self.associator.initialize(diffIm.src.schema, idFactory)
+                initializeSelector = True
+
+            srcWcs = diffIm.exp.getWcs()
+            mask = np.array(
+                [innerPatchBox.contains(coaddWcs.skyToPixel(srcWcs.pixelToSky(a.getCentroid())))
+                 for a in diffIm.src],
+                dtype=bool
+            )
+
+            src = diffIm.src[mask]
+            if len(src) == 0:
+                continue
+
+            self.log.info('Reading difference image %d %d, %s with %d possible sources' %
+                          (diffIm.visit, diffIm.ccd, diffIm.filter, len(src)))
+
+            footprints = []
+            region = calexp.getBBox(afwImage.PARENT)
+            for ii, rec in enumerate(src):
+                # transformations on large footprints can take a long time
+                # We truncate the footprint since we will rarely be interested
+                # in such large footprints
+                if rec.getFootprint().getArea() > self.config.maxFootprintArea:
+                    spans = afwGeom.SpanSet.fromShape(self.config.defaultFootprintRadius,
+                                                      afwGeom.Stencil.CIRCLE,
+                                                      afwGeom.Point2I(rec.getCentroid()))
+                    foot = afwDet.Footprint(spans)
+                    foot.addPeak(int(rec.getX()), int(rec.getY()), 1)
+                else:
+                    foot = rec.getFootprint()
+                footprints.append(foot.transform(srcWcs, coaddWcs, region))
+
+            self.associator.addCatalog(src, diffIm.filter, diffIm.visit, diffIm.ccd, footprints)
 
         result = self.associator.finalize(idFactory)
 
         if len(dataRefList) > 0 and result is not None:
             dataRefList[0].put(result, self.config.coaddName + 'Diff_diaObject')
             self.log.info('Total objects found %d' % len(result))
+
+    def selectExposures(self, patchRef, skyInfo=None, selectDataList=[]):
+        """!
+        @brief Select exposures to associate
+        Get the corners of the bbox supplied in skyInfo using @ref afwGeom.Box2D and convert the pixel
+        positions of the bbox corners to sky coordinates using @ref skyInfo.wcs.pixelToSky. Use the
+        @ref WcsSelectImagesTask_ "WcsSelectImagesTask" to select exposures that lie inside the patch
+        indicated by the dataRef.
+        @param[in] patchRef  data reference for sky map patch. Must include keys "tract", "patch",
+                             plus the camera-specific filter key (e.g. "filter" or "band")
+        @param[in] skyInfo   geometry for the patch; output from getSkyInfo
+        @return    a list of science exposures to coadd, as butler data references
+        """
+        if skyInfo is None:
+            skyInfo = self.getSkyInfo(patchRef)
+        cornerPosList = afwGeom.Box2D(skyInfo.bbox).getCorners()
+        coordList = [skyInfo.wcs.pixelToSky(pos) for pos in cornerPosList]
+        return self.select.runDataRef(patchRef, coordList, selectDataList=selectDataList).dataRefList
 
     def writeMetadata(self, dataRef):
         """We don't collect any metadata, so skip"""
