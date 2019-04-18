431468e96b0d1eb22c421388132e12e0c76f31eb
Bob Armstrong <rearmstr@gmail.com>
2019-04-18 21:25:26 +0200

    update to obs_lsst

--- config/imageDifferenceDriver.py
+++ config/imageDifferenceDriver.py
@@ -3,6 +3,6 @@ from lsst.utils import getPackageDir
 from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
 
 config.imageDifference.refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
-config.imageDifference.refObjLoader.load(os.path.join(getPackageDir('obs_lsstCam'), 'config', 'filterMap.py'))
+config.imageDifference.refObjLoader.load(os.path.join(getPackageDir('obs_lsst'), 'config', 'filterMap.py'))
 config.imageDifference.kernelSourcesFromRef = True
 config.ccdKey = 'detector'
