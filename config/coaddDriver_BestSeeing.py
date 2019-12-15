1ced0b7f2d6bcfb761abd63157ed3dd84f366d64
Dominique Fouchez <fouchez@cca007.in2p3.fr>
2019-12-15 13:01:46 +0100

    update to obs_lsst with bestseeing

---
+++ config/coaddDriver_BestSeeing.py
@@ -0,0 +1,24 @@
+# Load from sub-configurations
+import os.path
+
+from lsst.utils import getPackageDir
+
+from lsst.pipe.tasks.assembleCoadd import CompareWarpAssembleCoaddTask
+config.assembleCoadd.retarget(CompareWarpAssembleCoaddTask)
+
+for sub, filename in (("makeCoaddTempExp", "makeCoaddTempExp"),
+#                      ("backgroundReference", "backgroundReference"),
+                      ("assembleCoadd", "compareWarpAssembleCoadd"),
+                      ("processCoadd", "processCoadd")):
+    path = os.path.join(getPackageDir("obs_lsst"), "config", filename + ".py")
+    if os.path.exists(path):
+        getattr(config, sub).load(path)
+
+config.doBackgroundReference = False
+
+from lsst.pipe.tasks.selectImages import  BestSeeingSelectImagesTask
+config.select.retarget(BestSeeingSelectImagesTask)
+config.select.maxPsfFwhm=0.7/0.2
+
+from lsst.pipe.drivers.utils import NullSelectImagesTask
+config.assembleCoadd.select.retarget(NullSelectImagesTask)
